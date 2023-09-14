6f72c396573e7fb0336097368a0a9625692e2486
Kx <131256906+TanKx02@users.noreply.github.com>
2023-09-15 03:53:57 +0800

    community page

---
+++ app/src/main/java/com/example/owlingo/ui/community/CommunityViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.owlingo.ui.community
+
+import androidx.lifecycle.ViewModel
+
+class CommunityViewModel : ViewModel() {
+
+    private lateinit var questionList: MutableList<String>
+    private fun loadQuestions() {
+//        questionList = mutableListOf(
+//            Language("Java" , "Exp : 3 years"),
+//            Language("Kotlin" , "Exp : 2 years"),
+//            Language("Python" , "Exp : 4 years"),
+//            Language("JavaScript" , "Exp : 6 years"),
+//            Language("PHP" , "Exp : 1 years"),
+//            Language("CPP" , "Exp : 8 years"),
+//        )
+    }
+}
---
+++ app/src/main/java/com/example/owlingo/ui/community/QuestionFragment.kt
@@ -0,0 +1,22 @@
+package com.example.owlingo.ui.community
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.owlingo.R
+
+class QuestionFragment : Fragment() {
+
+    private lateinit var viewModel: CommunityViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_question, container, false)
+    }
+
+
+}
