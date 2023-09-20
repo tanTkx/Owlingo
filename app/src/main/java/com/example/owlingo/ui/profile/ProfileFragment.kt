c427fb638ba9342434a195c1b63659927bc9e81b
Kx <131256906+TanKx02@users.noreply.github.com>
2023-09-20 17:43:54 +0800

    solve confict

--- app/src/main/java/com/example/owlingo/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/owlingo/ui/profile/ProfileFragment.kt
@@ -1,22 +1,20 @@
-package com.example.owlingo
+package com.example.owlingo.ui.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.fragment.app.Fragment
+import com.example.owlingo.ARG_PARAM1
+import com.example.owlingo.ARG_PARAM2
+import com.example.owlingo.R
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LogoFragment.newInstance] factory method to
+ * Use the [ProfileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LogoFragment : Fragment() {
+class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +32,7 @@ class LogoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.logo_fragment, container, false)
+        return inflater.inflate(R.layout.profile_fragment, container, false)
     }
 
     companion object {
@@ -44,12 +42,12 @@ class LogoFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment LogoFragment.
+         * @return A new instance of fragment ProfileFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            LogoFragment().apply {
+            ProfileFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/owlingo/ui/profile/SignupFragment.kt
+++ app/src/main/java/com/example/owlingo/ui/profile/SignupFragment.kt
@@ -1,22 +1,20 @@
-package com.example.owlingo
+package com.example.owlingo.ui.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.fragment.app.Fragment
+import com.example.owlingo.ARG_PARAM1
+import com.example.owlingo.ARG_PARAM2
+import com.example.owlingo.R
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
+ * Use the [SignupFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginFragment : Fragment() {
+class SignupFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +32,7 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        return inflater.inflate(R.layout.signup_fragment, container, false)
     }
 
     companion object {
@@ -44,12 +42,12 @@ class LoginFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
+         * @return A new instance of fragment SignupFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
+            SignupFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
